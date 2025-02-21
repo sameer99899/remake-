d7ba69e4548c226391c1df89d3632378747010ae
sameer <sameer99899@gmail.com>
2025-02-21 15:38:05 +0530

    first commit

---
+++ Innovation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Innovation() {
+  return (
+    <div className='Innovation-main'>
+      <div className='innovation-content'>
+        <div className='innovative-heading'>Lighting Your World with Innovation and Efficiency</div>
+        <div className='innovative-description'>We Provide High-Performance LED Lights for Every Need</div>
+        <div className='innovative-buttons'>
+          <button className='shop-button'>Shop Now</button>
+          <button className='explore-button'>Explore Products</button>
+
+        </div>
+      </div>
+    </div>
+  )
+}
